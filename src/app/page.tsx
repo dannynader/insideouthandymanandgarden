5271c64f7faeea0e20839106c474a8195f0e9a5e
dannynader <dannynader9@gmail.com>
2025-04-08 20:26:51 +1000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import HomeOne from '@/components/homes/home';
+import Wrapper from '@/layouts/Wrapper';
+import React from 'react';
+
+const index = () => {
+  return (
+    <Wrapper>
+      <HomeOne /> 
+    </Wrapper>
+  );
+};
+
+export default index;
